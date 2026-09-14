59557916e59fb494e06396ab22281f2cbe83f00d
Sven Bischoff <sven@medienlampe.de>
2026-09-14 22:20:22 +0200

    Sets vite base for Github pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "https://medienlampe.github.io/Smorkinkboard/",
   plugins: [react()],
   server: {
     port: 3000,
