520aee3adcc2d6135d9a87764deda6eab47887e6
Sven A. Bischoff <medienlampe@users.noreply.github.com>
2026-09-23 11:52:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const pkg = JSON.parse(readFileSync(new URL("./package.json", import.meta.url), 
 };
 
 export default defineConfig({
-  base: "/Kinkburst/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
